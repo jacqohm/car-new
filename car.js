863990e868c7bf22e0946990f4df46e42f1c3674
Jacq Chin <jacq@ohmcreativestudio.com>
2016-10-10 14:06:14 +0800

    add files

---
+++ car.js
@@ -0,0 +1,9 @@
+var Car = function (color, convertible) {
+  this.color = color
+  this.speed = 0
+  this.convertible = convertible
+}
+// this.acelerate = acelerate
+// this.decelerate = decelerate
+
+module.exports = Car
